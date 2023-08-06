ce279fa485d224ae7d2ac02ae861f9b29e42a75a
Mary <omota4fidelity@gmail.com>
2023-08-07 00:24:51 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+document.querySelector('main');
+main.remove();
+//(newHeader.nodeName)
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+// document.body.append("newheader");
+const myName = "Mary";
+//newHeader.textContent = `${myName} is the champion`;
+newHeader.textContent = "Mary is the champion";
+document.body.appendChild(newHeader);
+
+
+
+
